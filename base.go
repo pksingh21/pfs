26f1811dcea8a31119414e6ba6a1e9b838793a46
Prateek Kumar Singh <20cs01003@iitbbs.ac.in>
2023-07-09 23:42:20 +0530

    added support for multi column multi directories

--- base.go
+++ base.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/mattn/go-runewidth"
@@ -22,7 +24,7 @@ const (
 	colorWhite   = termbox.ColorWhite
 
 	// Column widths
-	columnWidth = 20
+	columnWidth = 40
 	ellipsis    = "..."
 	// Spacing between columns
 	columnSpacing = 4
@@ -58,18 +60,23 @@ type FileItem struct {
 
 func main() {
 	err := termbox.Init()
+	showHiddenFiles := false
+	flag.BoolVar(&showHiddenFiles, "hidden", false, "Display hidden directories")
+	flag.BoolVar(&showHiddenFiles, "nonHidden", false, "Display non-hidden directories")
+	flag.Parse()
 	if err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
 
-	rand.Seed(time.Now().UnixNano())
-
+	seed := time.Now().UnixNano()
+	source := rand.NewSource(seed)
+	rand.New(source)
 	termbox.SetInputMode(termbox.InputEsc)
 
 	parentDir := "." // Replace with the desired parent directory path
 
-	items, err := getFileItems(parentDir)
+	items, err := getFileItems(parentDir, showHiddenFiles)
 	if err != nil {
 		panic(err)
 	}
@@ -77,11 +84,10 @@ func main() {
 	columnCount := len(columnColors)
 	totalWidth := columnCount*columnWidth + (columnCount-1)*columnSpacing
 	termWidth, termHeight := termbox.Size()
-	startX := (termWidth - totalWidth) / 2
+	startX := 0
 
 	visibleItems := termHeight - 1 // Subtract 1 for scrollbar
 	scrollOffset := 0
-	currentSelectionRow := 1
 
 	renderColumns(items, columnCount, startX, termWidth-scrollbarWidth, termHeight, visibleItems, scrollOffset, counter)
 
@@ -115,16 +121,16 @@ func main() {
 					termbox.Flush()
 				}
 			case termbox.KeyEnter:
-				if currentSelectionRow <= len(items) {
+				if counter <= len(items) {
 					// Perform the action for the selected item
 					// For example, navigate to the selected directory
-					selectedItem := items[currentSelectionRow-1]
+					selectedItem := items[counter-1]
 					if selectedItem.IsDir {
 						parentDir = selectedItem.FilePath
-						items, _ = getFileItems(parentDir)
-						currentSelectionRow = 1
+						items, _ = getFileItems(parentDir, showHiddenFiles)
 						scrollOffset = 0
 						counter = 1
+						startX = startX + columnWidth + columnSpacing
 						renderColumns(items, columnCount, startX, termWidth-scrollbarWidth, termHeight, visibleItems, scrollOffset, counter)
 						renderScrollbar(scrollbarWidth, termHeight, len(items), visibleItems, scrollOffset)
 						termbox.Flush()
@@ -157,16 +163,9 @@ func convertLongRune(longRune string, maxWidth int) []rune {
 }
 
 func renderColumns(items []FileItem, columnCount, startX, termWidth, termHeight, visibleItems, scrollOffset int, counter int) {
-	columnPositions = []int{}
-	for i := 0; i < columnCount; i++ {
-		pos := startX + i*(columnWidth+columnSpacing)
-		columnPositions = append(columnPositions, pos)
-	}
-
 	if counter-scrollOffset >= visibleItems {
 		scrollOffset = counter - visibleItems + 1
 	}
-
 	for i := 0; i < visibleItems; i++ {
 		index := i + scrollOffset
 		if index >= len(items) {
@@ -179,8 +178,7 @@ func renderColumns(items []FileItem, columnCount, startX, termWidth, termHeight,
 		row := i + 1
 
 		// Calculate column position based on index
-		columnIndex := i % columnCount
-		columnPosition := columnPositions[columnIndex]
+		columnPosition := startX
 
 		if item.RowNumber == counter {
 			item.IsCurrentSelection = true
@@ -195,35 +193,35 @@ func renderColumns(items []FileItem, columnCount, startX, termWidth, termHeight,
 func renderItem(item FileItem, x, y int, counter int) {
 	runes := convertLongRune(item.Name, columnWidth)
 	for i := 0; i < columnWidth; i++ {
-		termbox.SetCell(i, y, ' ', item.Color, termbox.ColorDefault)
+		termbox.SetCell(i+x, y, ' ', item.Color, termbox.ColorDefault)
 	}
 	// render counter variable used only for debugging purposes
-	// counterRune := []rune(strconv.Itoa(item.RowNumber) + "." + strconv.Itoa(counter))
-	// for i := 0; i < columnWidth-1; i++ {
-	// 	if i < len(counterRune) {
-	// 		termbox.SetCell(i, y, counterRune[i], item.Color, termbox.ColorDefault)
-	// 	}
-	// }
+	counterRune := []rune(strconv.Itoa(x))
+	for i := 0; i < columnWidth-1; i++ {
+		if i < len(counterRune) {
+			termbox.SetCell(x+i, y, counterRune[i], item.Color, termbox.ColorDefault)
+		}
+	}
 	// Display the row number
 	if item.IsCurrentSelection {
 		for i := 0; i < columnWidth-1; i++ {
 			if i < len(runes) {
-				termbox.SetCell(i, y, runes[i], item.Color, termbox.ColorDefault)
+				termbox.SetCell(i+len(counterRune)+x, y, runes[i], item.Color, termbox.ColorDefault)
 			} else if i == columnWidth-2 {
-				termbox.SetCell(i, y, item.Icon, item.Color, termbox.ColorDefault)
+				termbox.SetCell(i+x, y, item.Icon, item.Color, termbox.ColorDefault)
 			}
 		}
 
 		for i := 0; i < columnWidth; i++ {
-			termbox.SetBg(i, y, item.Color)
-			termbox.SetFg(i, y, termbox.ColorDefault)
+			termbox.SetBg(x+i, y, item.Color)
+			termbox.SetFg(x+i, y, termbox.ColorDefault)
 		}
 	} else {
 		for i := 0; i < columnWidth-1; i++ {
 			if i < len(runes) {
-				termbox.SetCell(i, y, runes[i], item.Color, termbox.ColorDefault)
+				termbox.SetCell(i+len(counterRune)+x, y, runes[i], item.Color, termbox.ColorDefault)
 			} else if i == columnWidth-2 {
-				termbox.SetCell(i, y, item.Icon, item.Color, termbox.ColorDefault)
+				termbox.SetCell(i+x, y, item.Icon, item.Color, termbox.ColorDefault)
 			}
 		}
 	}
@@ -247,21 +245,25 @@ func renderScrollbar(scrollbarWidth, termHeight, totalItems, visibleItems, scrol
 	}
 }
 
-func getFileItems(parentDir string) ([]FileItem, error) {
+func getFileItems(parentDir string, showHidden bool) ([]FileItem, error) {
 	files, err := os.ReadDir(parentDir)
 	if err != nil {
 		return nil, err
 	}
 
 	var items []FileItem
-	for i, file := range files {
+	nthitem := 0
+	for _, file := range files {
+		if !showHidden && file.Name()[0] == '.' {
+			continue // Skip hidden directories if showHidden is false
+		}
 		item := FileItem{
 			Name:      file.Name(),
 			IsDir:     file.IsDir(),
 			FilePath:  fmt.Sprintf("%s/%s", parentDir, file.Name()),
-			RowNumber: i + 1,
+			RowNumber: nthitem + 1,
 		}
-
+		nthitem++
 		if item.IsDir {
 			item.Icon = '📁'
 			item.Color = colorBlue
